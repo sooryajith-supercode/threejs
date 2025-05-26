3ba66a68954d61a0d0fc0f1b18fe7e0d5c2eaf5c
sooryajith-supercode <163262380+sooryajith-supercode@users.noreply.github.com>
2025-05-26 10:46:11 +0530

    Update Section2.jsx

--- src/app/3dModel/components/Section2.jsx
+++ src/app/3dModel/components/Section2.jsx
@@ -4,7 +4,6 @@ import { Canvas } from '@react-three/fiber'
 import * as styles from "../css/animation.module.css"
 import { Shoe } from '@/app/Models/Shoe'
 import { OrbitControls } from '@react-three/drei'
-import { Cloud } from '@/app/Models/Cloud'
 export default function Section2() {
   return (
     <section className={styles.section2} id='draco'>
